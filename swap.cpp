e8e8e012edaf1fcd4007fb92bdc0eaf92ddd4643
aditya mangal <adityamangal0202@gmail.com>
2020-08-29 08:26:53 -0700

    functions

---
+++ swap.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int swap(int &a, int &b);
+
+
+
+int main() {
+
+    int x=3;
+    int y=4;
+
+    cout<<"the value of x is "<<x<<" and the value of y is "<<y<<endl;
+    swap(x, y);
+    cout<<"the value of x is "<<x<<" and the value of y is "<<y<<endl;
+    return 0;
+}
+int swap(int &a, int &b)
+{
+
+    int temp = a;
+    a=b;
+    b=temp;
+
+}
+
